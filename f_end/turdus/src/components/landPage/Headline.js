14bf0794d508f3df257e0b56d251254905b84f00
danisanz <danielsanzelguer@gmail.com>
2022-05-10 18:30:50 +0200

    100522 headline bg

--- f_end/turdus/src/components/landPage/Headline.js
+++ f_end/turdus/src/components/landPage/Headline.js
@@ -2,8 +2,8 @@ function Headline() {
 
     return (
         <>
-            <div id="home" className="headline display-relative overflow-hidden py-3 p-md-5 m-md-3 h-100">
-                <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+            <div id="home" className="headline display-relative overflow-hidden py-3 p-md-5">
+                <div className="d-md-flex flex-md-equal w-100 my-3 ps-md-3">
                     <div className="me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-md-start text-white overflow-hidden flex-fill flex-grow-1">
                         <p>CLÍNICA VETERINARIA CON SERVICIO 24H LOS 365 DÍAS DEL AÑO</p>
                         <h1>Clínica Veterinaria TuClínica</h1>
@@ -13,14 +13,14 @@ function Headline() {
                                 especialidades para asegurar la salud de los peludos.
                             </p> */}
                         <p className="lead">
-                            <a role="button" className="btn btn-lg btn-primary fw-bold border-white" href="#contact">¿Dónde estamos?</a>
+                            <a role="button" className="btn btn-lg btn-outline-primary fw-bold border-0" href="#contact">¿Dónde estamos?</a>
                         </p>
                         <p className="lead">
-                            <a className="btn btn-lg btn-primary fw-bold border-white" href="#contact">Contacto</a>
+                            <a role="button" className="btn btn-lg btn-outline-primary fw-bold border-0" href="#contact">Contacto</a>
                         </p>
                     </div>
                     <div className="d-float headline-image">
-                            <img src="/img/headline1.jpg" className="img-fluid"/>
+                            {/* <img src="/img/headline2.jpg" className="img-fluid"/> */}
                     </div>
                 </div>
             </div>
